fb9f1ccda194aeb6c5af3706929f673317345be5
Devin Mounts and Ryan Putman <devin@>
2018-06-25 15:43:36 -0700

    create while loop that will evaluate if else if and else statement to return new random numbers until correct answer is reached.

--- hi-low.cs
+++ hi-low.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 class Guesses
 {
   private int _guesses;
-  // private int _randomNumber;
 
   public void SetGuess(int newGuesses)
   {
@@ -23,25 +22,46 @@ public class Program
   Guesses userGuess = new Guesses();
   Console.WriteLine("Would you like to play the guess a number game? Enter Yes or No");
   string response = Console.ReadLine();
+  int[] Range = {1, 101};
+  Random randm = new Random();
+  int rand_number = randm.Next(Range[0], Range[1]);
+  // List<Car> Cars = new List<Car>() { porsche, ford, lexus, mercedes };
   if (response == "yes" || response == "Yes")
   {
-    Console.WriteLine("Think of a number between 1 and 100.  When you're ready enter Okay.");
+  Console.WriteLine("Think of a number between 1 and 100.  When you're ready enter Okay.");
   }
   else
   {
-    return;
+  return;
   }
   response = Console.ReadLine();
   if (response == "Okay")
   {
-    Random randm = new Random();
-    int rand_number = randm.Next(1,101);
-    Console.WriteLine(rand_number);
+  Console.WriteLine("Is your number higher or lower than: " + rand_number + " (Enter: Higher/Lower/Correct)");
   }
-  else
+  response = Console.ReadLine();
+
+  while (true)
   {
-  Console.WriteLine("fail");
-  }
+    if (response == "Higher")
+    {
+    Range[0] = rand_number;
+    Console.WriteLine(Range[0]);
+    }
+    else if (response == "Lower")
+    {
+    Range[1] = rand_number;
+    Console.WriteLine(Range[1]);
+    }
+    else
+    {
+    Console.WriteLine("Success, I did it!");
+    break;
+    }
+    rand_number = randm.Next(Range[0], Range[1]);
+    Console.WriteLine("Is your number higher or lower than: " + rand_number + " (Enter: Higher/Lower/Correct)");
+    response = Console.ReadLine();
   }
 
+  }
 }
