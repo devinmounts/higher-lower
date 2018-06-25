f4264bd3b7362522349c760e69dd2d53e00df62c
Devin Mounts and Ryan Putman <ryan@>
2018-06-25 14:33:27 -0700

    add random number class to generate initial random number between 1-100

--- hi-low.cs
+++ hi-low.cs
@@ -1,12 +1,14 @@
 using System;
-using SystemsCollections.Generic;
+using System.Collections.Generic;
 
-class UserInput {
+class Guesses
+{
   private int _guesses;
+  // private int _randomNumber;
 
   public void SetGuess(int newGuesses)
   {
-   _guesses = int.Parse(newGuesses);
+   _guesses = newGuesses;
   }
   public int GetGuess()
   {
@@ -14,11 +16,32 @@ class UserInput {
   }
 }
 
-public class program
+public class Program
 {
   public static void Main()
   {
   Guesses userGuess = new Guesses();
-  Console.WriteLine()
+  Console.WriteLine("Would you like to play the guess a number game? Enter Yes or No");
+  string response = Console.ReadLine();
+  if (response == "yes" || response == "Yes")
+  {
+    Console.WriteLine("Think of a number between 1 and 100.  When you're ready enter Okay.");
   }
+  else
+  {
+    return;
+  }
+  response = Console.ReadLine();
+  if (response == "Okay")
+  {
+    Random randm = new Random();
+    int rand_number = randm.Next(1,101);
+    Console.WriteLine(rand_number);
+  }
+  else
+  {
+  Console.WriteLine("fail");
+  }
+  }
+
 }
