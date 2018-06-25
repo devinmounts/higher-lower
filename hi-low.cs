c3fa7ea312935c57883c71bcefa4c98d0b71e41d
Devin Mounts and Ryan Putman <ryan@>
2018-06-25 16:14:27 -0700

    add function for computer tothink of number and user guess.

--- hi-low.cs
+++ hi-low.cs
@@ -25,20 +25,19 @@ public class Program
   int[] Range = {1, 101};
   Random randm = new Random();
   int rand_number = randm.Next(Range[0], Range[1]);
-  // List<Car> Cars = new List<Car>() { porsche, ford, lexus, mercedes };
+
   if (response == "yes" || response == "Yes")
   {
-  Console.WriteLine("Think of a number between 1 and 100.  When you're ready enter Okay.");
+  Console.WriteLine("Do you want the computer to guess the number?  When you're ready enter Yes or No.");
   }
   else
   {
   return;
   }
   response = Console.ReadLine();
-  if (response == "Okay")
+  if (response == "No")
   {
   Console.WriteLine("Is your number higher or lower than: " + rand_number + " (Enter: Higher/Lower/Correct)");
-  }
   response = Console.ReadLine();
 
   while (true)
@@ -53,15 +52,42 @@ public class Program
     Range[1] = rand_number;
     Console.WriteLine(Range[1]);
     }
-    else
+    else if (response == "Correct")
     {
     Console.WriteLine("Success, I did it!");
     break;
+    }
+    else
+    {
+    Console.WriteLine("Oops, enter in a valid response...");
     }
     rand_number = randm.Next(Range[0], Range[1]);
     Console.WriteLine("Is your number higher or lower than: " + rand_number + " (Enter: Higher/Lower/Correct)");
     response = Console.ReadLine();
   }
-
+  }
+  else if (response == "Yes")
+  {
+  Console.WriteLine("I have my number, go ahead and start guessing");
+  int inputtedValue =  int .Parse(Console.ReadLine());
+    while (true)
+    {
+      if (inputtedValue == rand_number)
+      {
+      Console.WriteLine("Wow, you guessed my number!!!");
+      break;
+      }
+      else if (inputtedValue < rand_number)
+      {
+      Console.WriteLine("Guess Again, my number is greater than " + inputtedValue + " .");
+      inputtedValue = int .Parse(Console.ReadLine());
+      }
+      else if (inputtedValue > rand_number)
+      {
+      Console.WriteLine("Guess Again, my number is lower than " + inputtedValue + " .");
+      inputtedValue = int .Parse(Console.ReadLine());
+      }
+    }
+  }
   }
 }
